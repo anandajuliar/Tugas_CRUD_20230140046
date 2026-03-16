458d31c4b9e73a165ff30c25bff1011bac72491e
anandajuliar <anandajulia001@gmail.com>
2026-03-16 11:30:53 +0700

    tambah KtpRepository untuk interaksi langsung ke MySQL

---
+++ src/main/java/com/tugas/tugasktp/repository/KtpRepository.java
@@ -0,0 +1,10 @@
+package com.tugas.tugasktp.repository;
+
+import com.tugas.tugasktp.model.Ktp;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface KtpRepository extends JpaRepository<Ktp, Integer> {
+    boolean existsByNomorKtp(String nomorKtp);
+}
