df009bc831cd619c6055ae1a186c47a1f9f817f6
anandajuliar <anandajulia001@gmail.com>
2026-03-16 10:49:50 +0700

    Setup awal project Spring Boot untuk CRUD KTP

---
+++ src/main/java/com/tugas/tugasktp/TugasKtpApplication.java
@@ -0,0 +1,13 @@
+package com.tugas.tugasktp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TugasKtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TugasKtpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tugas/tugasktp/TugasKtpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tugas.tugasktp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TugasKtpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
